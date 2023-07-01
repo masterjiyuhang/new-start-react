2afe5df1587d132de9fe98a77fb21748d2ab1250
masterjiyuhang <syjzjyh@163.com>
2023-07-02 00:24:07 +0800

    feat: 🚀 更新引导页

--- src/views/assembly/guide/index.tsx
+++ src/views/assembly/guide/index.tsx
@@ -1,32 +1,43 @@
 import useIntroJs from "@/hooks/useIntro";
 import { Button } from "antd";
-import React from "react";
+import "./index.scss";
 
 const guide = () => {
 	const { intro } = useIntroJs();
 	console.log(intro);
 
-	console.log(document.querySelector("#step1"));
+	const startIntro = () => {
+		console.log(document.querySelector("#step1"));
+		intro.setOptions({
+			steps: [
+				{
+					element: document.querySelector("#step1") as HTMLElement, // 定位到相应的元素位置，如果不设置element，则默认展示在屏幕中央
+					title: "Welcome", // 标题
+					intro: "Hello World! 👋" // 内容
+				},
+				{
+					element: document.querySelector("#step2") as HTMLElement, // 定位到相应的元素位置，如果不设置element，则默认展示在屏幕中央
+					title: "Welcome", // 标题
+					intro: "Hello World! 👋" // 内容
+				}
+			]
+		});
 
-	intro.setOptions({
-		steps: [
-			{
-				element: document.querySelector("#step1") as HTMLElement, // 定位到相应的元素位置，如果不设置element，则默认展示在屏幕中央
-				title: "Welcome", // 标题
-				intro: "Hello World! 👋" // 内容
-			}
-		]
-	});
+		intro.start();
+	};
 
 	return (
-		<div>
-			<div id="step1" className="ls">
+		<div className="card content-box">
+			<div id="step1" className="text">
 				guide 引导页
 			</div>
-			<div data-title="Welcome!" data-intro="Hello World!">
-				hello!
+			<div id="step2" className="text">
+				hello setup2 !{" "}
 			</div>
-			<Button onClick={() => intro.start()}>kaishi</Button>
+			<Button onClick={startIntro} className="intro-btn">
+				{" "}
+				开始指引{" "}
+			</Button>
 		</div>
 	);
 };
