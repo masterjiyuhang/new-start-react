4678eb2852f40836147a0c7e2ff0c07ef7c76f32
erhang <syjzjyh@163.com>
2023-07-03 10:23:13 +0800

    feat: 🚀 完善浏览器全屏功能

--- src/layouts/components/Header/components/FullScreen.tsx
+++ src/layouts/components/Header/components/FullScreen.tsx
@@ -1,8 +1,32 @@
-import { FullscreenOutlined } from "@ant-design/icons";
-import React from "react";
+import { FullscreenOutlined, FullscreenExitOutlined } from "@ant-design/icons";
+import { message } from "antd";
+import React, { useEffect, useState } from "react";
+import screenfull from "screenfull";
 
 const FullScreen = () => {
-	return <FullscreenOutlined className="icon-style" />;
+	const [fullScreen, setFullScreen] = useState(false);
+
+	useEffect(() => {
+		screenfull.on("change", () => {
+			if (screenfull.isFullscreen) setFullScreen(true);
+			else setFullScreen(false);
+			return () => screenfull.off("change", () => {});
+		});
+	}, []);
+
+	const handleFullScreen = () => {
+		if (!screenfull.isEnabled) message.warning("当前您的浏览器不支持全屏 ❌");
+		screenfull.toggle();
+	};
+	return (
+		<>
+			{fullScreen ? (
+				<FullscreenExitOutlined className="icon-style" onClick={handleFullScreen} />
+			) : (
+				<FullscreenOutlined className="icon-style" onClick={handleFullScreen} />
+			)}
+		</>
+	);
 };
 
 export default FullScreen;
