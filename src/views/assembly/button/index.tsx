65fcbdf06a3a8bbf1c364a7b2d0e146f6bd9d0a2
纪宇航 <jiyuhang@jctrans.net>
2023-08-22 14:08:18 +0800

    feat: init icon font, update button demo

--- src/views/assembly/button/index.tsx
+++ src/views/assembly/button/index.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styles from "./index.module.scss";
 
 const SelfButton = () => {
-	return <div>self button</div>;
+	return (
+		<div className={styles.container}>
+			<button className={styles["btn-1"]}>
+				<div>
+					<span>show more</span>
+					<span>View More</span>
+				</div>
+			</button>
+
+			<button type="button" className={styles.btn2}>
+				View More
+			</button>
+		</div>
+	);
 };
 
 export default SelfButton;
