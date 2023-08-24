d847d7a6662aebaa1d4ccf852edcfd7486cefb41
纪宇航 <jiyuhang@jctrans.net>
2023-08-24 18:01:40 +0800

    fix: 功能调整

--- src/views/assembly/button/index.tsx
+++ src/views/assembly/button/index.tsx
@@ -1,29 +1,36 @@
 import React from "react";
 import styles from "./index.module.scss";
 import HoverButton from "./src/HoverButton";
+import ExpandButton from "./src/ExpandButton";
 
 const SelfButton = () => {
 	return (
 		<div className={styles.container}>
-			<button className={styles["btn-1"]}>
-				<div>
-					<span>show more</span>
-					<span>View More</span>
-				</div>
-			</button>
+			<div className={styles.innerBox}>
+				<button className={styles["btn-1"]}>
+					<div>
+						<span>show more</span>
+						<span>View More</span>
+					</div>
+				</button>
 
-			<button type="button" className={styles.btn2}>
-				View More
-			</button>
+				<button type="button" className={styles.btn2}>
+					View More
+				</button>
 
-			<button type="button" className={styles.btn3}>
-				彩色的我
-			</button>
-			<button type="button" className={styles.btn4}>
-				有边的我
-			</button>
+				<button type="button" className={styles.btn3}>
+					彩色的我
+				</button>
+				<button type="button" className={styles.btn4}>
+					有边的我
+				</button>
+			</div>
 
-			<HoverButton />
+			<div style={{ marginTop: "20px" }}>
+				<HoverButton />
+			</div>
+
+			<ExpandButton />
 		</div>
 	);
 };
---
+++ src/views/assembly/button/src/ExpandButton/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./expand.module.scss";
+
+const ExpandButton = () => {
+	return (
+		<div className={styles.container}>
+			<button className={styles["basic-button"]}>
+				<span className={styles.circle}>
+					<span className={`iconfont icon-follow ${styles.icon}`}></span>
+				</span>
+
+				<span className={styles.text}>Expand Button</span>
+			</button>
+		</div>
+	);
+};
+
+export default ExpandButton;
